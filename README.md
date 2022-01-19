d779badcb2919ff5c31fdcf46482eb2bfdad9df9
Sufian Suni <sufiansuni@gmail.com>
2022-01-19 19:50:31 +0800

    day36 step3 & optional format

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Python-100DaysOfCode
- 
+
 This repository contains projects done during the following course:
 
 > 100 Days of Code - The Complete Python Pro Bootcamp for 2021 \
@@ -7,27 +7,29 @@ This repository contains projects done during the following course:
 > Udemy.com
 
 ## Projects
-| Day | Project Name | Remarks |
-|--|--|--|
-| 15 | Coffee Machine | A Coffee Machine Simulation |
-| 16 | Coffee Machine | Previous Project re-structured with OOP |
-| 17 | Quiz Project | Quiz App with OOP |
-| 18 | Hirst Paiting Project | Image Creation Project,  Turtle Graphics |
-| 19 | Turtle Race Game | Betting Racing Game, Turtle Graphics |
-| 20 | Snake Game | Classic Snake Game, Turtle Graphics |
-| 21 | Snake Game | Previous Project with Inheritence |
-| 22 | Pong Game | Classic Pong Game, Turtle Graphics |
-| 23 | Turtle Crossing Game | Capstone Project, Turtle Graphics |
-| 24 | Mail Merge | Email Generator, uses templates from files |
-| 25 | 50 States Game | 50 States Game, Turtle Graphics, Pandas |
-| 26 | NATO Alphabet | Word Mapper App |
-| 27 | Miles to Km | Unit Converter App, Tkinter |
-| 28 | Pomodoro App | Task Timer App, Tkinter |
-| 29 | Password Manager App | Password Manager App, Tkinter |
-| 30 | Error & Exceptions Demo | Example of Error Handling |
-| 31 | FlashCards App | Memory App, Tkinter |
-| 32 | Birthday Mailer App | Automatic Email Sender |
-| 33 | Kanye Quotes App | Calls API and display's a random Kanye quote |
-| 33 | ISS Tracker | Tracks the position of ISS via API, email when user is near |
-| 34 | Quizzler | Quiz Game, Improvement from day17, API call, Tkinter, OOP |
-| 35 | RainChecker App | Sends SMS if rain in the next 12 hours, Open Weather, Twilio |
+
+| Day | Project Name            | Remarks                                                            |
+| --- | ----------------------- | ------------------------------------------------------------------ |
+| 15  | Coffee Machine          | A Coffee Machine Simulation                                        |
+| 16  | Coffee Machine          | Previous Project re-structured with OOP                            |
+| 17  | Quiz Project            | Quiz App with OOP                                                  |
+| 18  | Hirst Paiting Project   | Image Creation Project, Turtle Graphics                            |
+| 19  | Turtle Race Game        | Betting Racing Game, Turtle Graphics                               |
+| 20  | Snake Game              | Classic Snake Game, Turtle Graphics                                |
+| 21  | Snake Game              | Previous Project with Inheritence                                  |
+| 22  | Pong Game               | Classic Pong Game, Turtle Graphics                                 |
+| 23  | Turtle Crossing Game    | Capstone Project, Turtle Graphics                                  |
+| 24  | Mail Merge              | Email Generator, uses templates from files                         |
+| 25  | 50 States Game          | 50 States Game, Turtle Graphics, Pandas                            |
+| 26  | NATO Alphabet           | Word Mapper App                                                    |
+| 27  | Miles to Km             | Unit Converter App, Tkinter                                        |
+| 28  | Pomodoro App            | Task Timer App, Tkinter                                            |
+| 29  | Password Manager App    | Password Manager App, Tkinter                                      |
+| 30  | Error & Exceptions Demo | Example of Error Handling                                          |
+| 31  | FlashCards App          | Memory App, Tkinter                                                |
+| 32  | Birthday Mailer App     | Automatic Email Sender                                             |
+| 33  | Kanye Quotes App        | Calls API and display's a random Kanye quote                       |
+| 33  | ISS Tracker             | Tracks the position of ISS via API, email when user is near        |
+| 34  | Quizzler                | Quiz Game, Improvement from day17, API call, Tkinter, OOP          |
+| 35  | RainChecker App         | Sends SMS if rain in the next 12 hours, Open Weather, Twilio       |
+| 36  | StockNews App           | Sends SMS if Tesla stocks change > 5%, Alpha Vantage, News, Twilio |
--- day-36-StockNews/main.py
+++ day-36-StockNews/main.py
@@ -1,7 +1,9 @@
 from dotenv import load_dotenv
 import os
 import requests
-import datetime
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
+import math
 
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -13,6 +15,18 @@ load_dotenv()
 
 ALPHA_VANTAGE_API_KEY = os.environ.get("ALPHA_VANTAGE_API_KEY")
 NEWS_API_KEY = os.environ.get("NEWS_API_KEY")
+
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+# proxy_client = TwilioHttpClient(
+#     proxy={'http': os.environ['http_proxy'], 'https': os.environ['https_proxy']})
+
+client = Client(account_sid, auth_token)
+# client = Client(account_sid, auth_token, http_client=proxy_client)
+
+TWILIO_PHONE_NUMBER = os.environ['TWILIO_PHONE_NUMBER']
+MY_PHONE_NUMBER = os.environ['MY_PHONE_NUMBER']
+
 
 # STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
@@ -39,7 +53,8 @@ day_before_yesterday_closing = float(stock_data_list[1]["4. close"])
 # print(day_before_yesterday_closing)
 
 # TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
-positive_diff = abs(yesterday_closing - day_before_yesterday_closing)
+raw_diff = yesterday_closing - day_before_yesterday_closing
+positive_diff = abs(raw_diff)
 
 # TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
 percent_diff = positive_diff/day_before_yesterday_closing * 100
@@ -47,24 +62,23 @@ percent_diff = positive_diff/day_before_yesterday_closing * 100
 
 # TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
 if percent_diff > 5:
-    print("Get News")
+    # print("Get News")
 
+    # STEP 2: https://newsapi.org/
+    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
 
-# STEP 2: https://newsapi.org/
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+    # TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
+    news_api_params = {
+        "q": COMPANY_NAME,
+        "sortBy": "popularity",
+        "apikey": NEWS_API_KEY
+    }
 
-# TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
-news_api_params = {
-    "q": COMPANY_NAME,
-    "sortBy": "popularity",
-    "apikey": NEWS_API_KEY
-}
-
-news_response = requests.get(url=NEWS_ENDPOINT, params=news_api_params)
-news_response.raise_for_status()
+    news_response = requests.get(url=NEWS_ENDPOINT, params=news_api_params)
+    news_response.raise_for_status()
 
 # TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
-news_data = news_response.json()["articles"][:3]
+    news_data = news_response.json()["articles"][:3]
 
 # print(news_data)
 
@@ -72,17 +86,43 @@ news_data = news_response.json()["articles"][:3]
 # to send a separate message with each article's title and description to your phone number.
 
 # TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
+    formatted_articles = [{"title": article["title"],
+                           "description": article["description"]} for article in news_data]
+# print(formatted_articles)
 
 # TODO 9. - Send each article as a separate message via Twilio.
+    for article in formatted_articles:
+        if raw_diff > 0:
+            change_symbol = "🔺"
+        else:
+            change_symbol = "🔻"
 
+        headline = article["title"]
+        brief = article["description"]
+
+        body = f"\nTSLA: {change_symbol}{math.floor(percent_diff)}%\n"\
+            f"Headline: {headline}\n"\
+            f"Brief: {brief}"
+
+        # print(body)
+
+        message = client.messages \
+            .create(
+                body=body,
+                from_=TWILIO_PHONE_NUMBER,
+                to=MY_PHONE_NUMBER
+            )
+        print("SMS Sent. Message SID: ", message.sid)
 
 # Optional TODO: Format the message like this:
 """
 TSLA: 🔺2%
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
 Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+
 or
-"TSLA: 🔻5%
+
+TSLA: 🔻5%
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
 Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
 """
