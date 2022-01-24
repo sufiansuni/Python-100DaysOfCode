647ebf04009675856b845d0f66ae322624250b04
Sufian Suni <sufiansuni@gmail.com>
2022-01-24 21:34:07 +0800

    day38 Step 2 of 6 done

--- day-38-ExerciseTracker/main.py
+++ day-38-ExerciseTracker/main.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+import requests
 
 load_dotenv()
 
@@ -7,3 +8,26 @@ PIXELA_TOKEN = os.environ.get("PIXELA_TOKEN")
 
 NUTRITIONIX_APP_ID = os.environ.get("NUTRITIONIX_APP_ID")
 NUTRITIONIX_API_KEY = os.environ.get("NUTRITIONIX_API_KEY")
+
+
+def get_exercise_info():
+    endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+    params = {
+        "query": input("Tell me which exercises you did: ")
+    }
+
+    headers = {
+        "x-app-id": NUTRITIONIX_APP_ID,
+        "x-app-key": NUTRITIONIX_API_KEY
+    }
+
+    response = requests.post(
+        url=endpoint,
+        json=params,
+        headers=headers)
+
+    response.raise_for_status()
+    print(response.text)
+
+
+get_exercise_info()
