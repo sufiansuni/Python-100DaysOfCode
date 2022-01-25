8f8fdc1a58ff234081bcf9648f14bb479a990d4e
Sufian Suni <85665908+sufiansuni@users.noreply.github.com>
2022-01-25 22:39:25 +0800

    day39 Step 2 of 5 done

--- day-39-FlightDealFinder/data_manager.py
+++ day-39-FlightDealFinder/data_manager.py
@@ -1,3 +1,30 @@
+import requests
+
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    # This class is responsible for talking to the Google Sheet.
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        endpoint = "https://api.sheety.co/2441cf3e4d3f8a33fc100d25122c24f8/flightDeals/prices"
+
+        response = requests.get(url=endpoint)
+
+        response.raise_for_status()
+        data = response.json()
+        self.destination_data = data["prices"]
+        return self.destination_data
+
+    def update_destination_codes(self):
+        for row in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": row["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"https://api.sheety.co/2441cf3e4d3f8a33fc100d25122c24f8/flightDeals/prices/{row['id']}",
+                json=new_data
+            )
+            print(response.text)
--- day-39-FlightDealFinder/main.py
+++ day-39-FlightDealFinder/main.py
@@ -1,1 +1,18 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+# This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from data_manager import DataManager
+from pprint import pprint
+
+data_manager = DataManager()
+sheet_data = data_manager.get_destination_data()
+
+# pprint(sheet_data)
+
+for row in sheet_data:
+    if row['iataCode'] == '':
+        row['iataCode'] = "TESTING"
+
+data_manager.destination_data = sheet_data
+
+# pprint (data_manager.destination_data)
+
+data_manager.update_destination_codes()
