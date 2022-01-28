195ce4b93c9a2d13053056b355549f1e2c19b127
Sufian Suni <85665908+sufiansuni@users.noreply.github.com>
2022-01-28 22:18:35 +0800

    day 39 Step 5 of 5 done

--- day-39-FlightDealFinder/main.py
+++ day-39-FlightDealFinder/main.py
@@ -1,6 +1,7 @@
 # This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 from data_manager import DataManager
 from flight_search import FlightSearch
+from notification_manager import NotificationManager
 from pprint import pprint
 from datetime import datetime, timedelta
 
@@ -33,6 +34,9 @@ if sheet_data[0]["iataCode"] == "":
 tomorrow = datetime.now() + timedelta(days=1)
 six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
 
+
+notification_manager = NotificationManager()
+
 for destination in sheet_data:
     flight = flight_search.check_flights(
         ORIGIN_CITY_IATA,
@@ -40,3 +44,8 @@ for destination in sheet_data:
         from_time=tomorrow,
         to_time=six_month_from_today
     )
+
+    if flight.price < destination["lowestPrice"]:
+        notification_manager.send_sms(
+            message=f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
+        )
