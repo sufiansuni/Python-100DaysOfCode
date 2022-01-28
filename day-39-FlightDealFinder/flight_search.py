d14f1a436656f3f87b170d2e005179935e3db89c
Sufian Suni <85665908+sufiansuni@users.noreply.github.com>
2022-01-28 22:10:34 +0800

    day39 Step 4 of 5 done

--- day-39-FlightDealFinder/flight_search.py
+++ day-39-FlightDealFinder/flight_search.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import os
 import requests
 from pprint import pprint
-
+from flight_data import FlightData
 load_dotenv()
 
 TEQUILA_API_KEY = os.environ.get("TEQUILA_API_KEY")
@@ -31,3 +31,43 @@ class FlightSearch:
         response.raise_for_status()
         data = response.json()
         return data["locations"][0]["code"]
+
+
+    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+        headers = {"apikey": TEQUILA_API_KEY}
+        query = {
+            "fly_from": origin_city_code,
+            "fly_to": destination_city_code,
+            "date_from": from_time.strftime("%d/%m/%Y"),
+            "date_to": to_time.strftime("%d/%m/%Y"),
+            "nights_in_dst_from": 7,
+            "nights_in_dst_to": 28,
+            "flight_type": "round",
+            "one_for_city": 1,
+            "max_stopovers": 0,
+            "curr": "GBP"
+        }
+
+        response = requests.get(
+            url=f"https://tequila-api.kiwi.com/v2/search",
+            headers=headers,
+            params=query,
+        )
+
+        try:
+            data = response.json()["data"][0]
+        except IndexError:
+            print(f"No flights found for {destination_city_code}.")
+            return None
+
+        flight_data = FlightData(
+            price=data["price"],
+            origin_city=data["route"][0]["cityFrom"],
+            origin_airport=data["route"][0]["flyFrom"],
+            destination_city=data["route"][0]["cityTo"],
+            destination_airport=data["route"][0]["flyTo"],
+            out_date=data["route"][0]["local_departure"].split("T")[0],
+            return_date=data["route"][1]["local_departure"].split("T")[0]
+        )
+        print(f"{flight_data.destination_city}: £{flight_data.price}")
+        return flight_data
--- day-39-FlightDealFinder/main.py
+++ day-39-FlightDealFinder/main.py
@@ -2,6 +2,7 @@
 from data_manager import DataManager
 from flight_search import FlightSearch
 from pprint import pprint
+from datetime import datetime, timedelta
 
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
@@ -18,3 +19,24 @@ data_manager.destination_data = sheet_data
 # pprint (data_manager.destination_data)
 
 data_manager.update_destination_codes()
+
+flight_search = FlightSearch()
+
+ORIGIN_CITY_IATA = "LON"
+
+if sheet_data[0]["iataCode"] == "":
+    for row in sheet_data:
+        row["iataCode"] = flight_search.get_destination_code(row["city"])
+    data_manager.destination_data = sheet_data
+    data_manager.update_destination_codes()
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+for destination in sheet_data:
+    flight = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_today
+    )
