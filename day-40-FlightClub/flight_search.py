270fe2d6ed73d261cf5a9c6b9c90ffce35563499
Sufian Suni <85665908+sufiansuni@users.noreply.github.com>
2022-01-30 18:12:15 +0800

    day40 non-direct flights

--- day-40-FlightClub/flight_search.py
+++ day-40-FlightClub/flight_search.py
@@ -57,8 +57,28 @@ class FlightSearch:
             data = response.json()["data"][0]
             print(f"{destination_city_code} {data['price']}")
         except IndexError:
-            print(f"No flights found for {destination_city_code}.")
-            return None
+            # print(f"No flights found for {destination_city_code}.")
+            # return None
+            query["max_stopovers"] = 1
+            response = requests.get(
+                url=f"https://tequila-api.kiwi.com//v2/search",
+                headers=headers,
+                params=query,
+            )
+            data = response.json()["data"][0]
+            pprint(data)
+            flight_data = FlightData(
+                price=data["price"],
+                origin_city=data["route"][0]["cityFrom"],
+                origin_airport=data["route"][0]["flyFrom"],
+                destination_city=data["route"][1]["cityTo"],
+                destination_airport=data["route"][1]["flyTo"],
+                out_date=data["route"][0]["local_departure"].split("T")[0],
+                return_date=data["route"][2]["local_departure"].split("T")[0],
+                stop_overs=1,
+                via_city=data["route"][0]["cityTo"]
+            )
+            return flight_data
         else:
             flight_data = FlightData(
                 price=data["price"],
